1b7e231fbd5b612629dc4877fbbb98e986dc46d8
Trevor Low <tlow94@uw.edu>
2020-05-01 02:34:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,6 +48,6 @@ def self.find_by_name(name)
   sql = <<-SQL
   SELECT*FROM students WHERE name = ? LIMIT 1
   SQL
-  DB[:conn].execute(sql).map {|row| self.new_from_db(row)}.first
+  DB[:conn].execute(sql, name).map {|row| self.new_from_db(row)}.first
 end
 end
